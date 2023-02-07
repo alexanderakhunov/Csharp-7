875ce1b3ef40d81ad2bd83ef5998af3c27cac453
AlexanderAkh <sancheek240@mail.ru>
2023-02-07 17:34:10 +0300

    add Task47.52

---
+++ Task47/Program.cs
@@ -0,0 +1,32 @@
+﻿double[,] FillMatrix(int rows, int cols)
+{
+    double[,] matr = new double[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = Math.Round(rand.Next(-10, 10) + rand.NextDouble(),1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(double [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        System.Console.Write((matr[i, j] + "\t"));
+    }
+    System.Console.WriteLine();
+}
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+
---
+++ Task52/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] FillMatrix(int rows, int cols)
+{
+    int[,] matr = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        System.Console.Write((matr[i, j] + "\t"));
+    }
+    System.Console.WriteLine();
+}
+}
+
+int SummMatrix(int[,] mart)
+{
+    int result = 0;
+    for (int i = 0; i < mart.GetLength(0); i++)
+    {
+        for (int j = 0; j < mart.GetLength(1); j++)
+        {
+            if((j == 0))
+            {
+                result += mart[i,j];
+            }
+        }
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine(SummMatrix(matrix));
